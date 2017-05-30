eded4ba098066249cafc67c13c1def7e3ac2bc10
crekowski <cornelius.vonrekowski@gmail.com>
2017-05-30 16:12:50 +0200

    Move training and prediction UI to UI module

--- preprocessing.py
+++
@@ -1,8 +0,0 @@
-import sklearn.decomposition as skdecomp
-
-def perform_pca(data, n):
-    pca = skdecomp.PCA(n_components=n)
-    features = data.drop('# label', axis=1).compute()
-    pca.fit(features)
-    print('PCA with {} principal components computed.'.format(n))
-    return pca
--- widget_ui.py
+++ widget_ui.py
@@ -1,8 +1,15 @@
 from ipywidgets import *
 from IPython.display import display
+import numpy as np
+import time
 from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix
+import sklearn.decomposition as skdecomp
 import data_utils
-import preprocessing
+
+
+#### Data Preparation ####
 
 def subsampling_ui(all_data, output_data):
     sampling_rate = BoundedFloatText(
@@ -11,13 +18,25 @@ def subsampling_ui(all_data, output_data):
         max=1.0,
         description='Sampling rate:'
     )
-
-    i = interact(data_utils.subsample_data, 
+    i = interact(
+        data_utils.subsample_data, 
         data=fixed(all_data), 
         r=sampling_rate, 
-        output_data=fixed(output_data))
+        output_data=fixed(output_data)
+    )
 
-def pca_ui(data):
+
+#### Preprocessing ####
+
+def perform_pca(preprocessors, data, n):
+    pca = skdecomp.PCA(n_components=n)
+    features = data.drop('# label', axis=1).compute()
+    pca.fit(features)
+    print('PCA with {} principal components computed.'.format(n))
+
+    preprocessors['pca'] = pca
+
+def pca_ui(preprocessors, data):
     ncomponents = IntSlider(
         value=5,
         min=1,
@@ -27,18 +46,16 @@ def pca_ui(data):
         continuous_update=False,
         layout=Layout(width='80%')
     )
-
-    i = interactive(preprocessing.perform_pca, 
+    i = interactive(
+        perform_pca, 
+        preprocessors=fixed(preprocessors),
         data=fixed(data.train_data), 
-        n=ncomponents)
+        n=ncomponents
+    )
     display(i)
 
-    # Wrap resulting transformation in a function so that it is only
-    # evaluated in the end, because there may be intermediate results.
-    def init_pca():
-        return i.result
 
-    return init_pca
+#### Classifiers ####
 
 def set_max_iter(m, max_iter):
     max_iter[0] = m
@@ -74,7 +91,7 @@ def configure_hidden_layers(n, hidden_layer_box, hidden_layers):
     )
     display(hidden_layer_box[0])
 
-def mlp_ui():
+def mlp_ui(classifiers):
     hidden_layer_box = [None]
     hidden_layers = [20, 20]
     max_iter = [30]
@@ -88,9 +105,11 @@ def mlp_ui():
         continuous_update=False,
         layout=Layout(width='80%')
     )
-    i = interact(set_max_iter, 
+    i = interact(
+        set_max_iter, 
         m=max_iter_slider,
-        max_iter=fixed(max_iter))
+        max_iter=fixed(max_iter)
+    )
 
     nhidden = IntSlider(
         value=2,
@@ -101,11 +120,12 @@ def mlp_ui():
         continuous_update=False,
         layout=Layout(width='80%')
     )
-
-    i = interact(configure_hidden_layers, 
+    i = interact(
+        configure_hidden_layers, 
         n=nhidden,
         hidden_layer_box=fixed(hidden_layer_box),
-        hidden_layers=fixed(hidden_layers))
+        hidden_layers=fixed(hidden_layers)
+    )
 
     def init_mlp():
         mlp = MLPClassifier(hidden_layer_sizes=hidden_layers, 
@@ -114,12 +134,225 @@ def mlp_ui():
         mlp.train = mlp.fit
         return mlp
 
-    return init_mlp
+    classifiers['mlp'] = {'init': init_mlp}
 
-def nb_ui():
+def nb_ui(classifiers):
     def init_nb():
         naiveBayes = GaussianNB(priors=[0.5, 0.5])
         naiveBayes.train = lambda x, y: naiveBayes.partial_fit(x, y, classes=[1,0])
         return naiveBayes
 
-    return init_nb
+    classifiers['nb'] = {'init': init_nb}
+
+
+#### Training ####
+
+def train_on_window(preprocessors, apply_pca, classifier, window):
+    labels = window[:, 0]
+    features = window[:, 1:29]
+    
+    # Optionally apply PCA
+    if apply_pca:
+        features = preprocessors['pca'].transform(features)
+    
+    # Train classifier
+    classifier.fit(features, labels)
+
+
+def perform_training(
+    data, preprocessors, apply_pca, window_size, classifiers, 
+    classifier_name):
+    
+    # Initialize classifier
+    if classifier_name == 'Multilayer Perceptron':
+        classifier = classifiers['mlp']['init']()
+        classifiers['mlp']['trained'] = classifier
+    else:
+        classifier = classifiers['nb']['init']()
+        classifiers['nb']['trained'] = classifier
+    
+    print('Training {}'.format(classifier))
+    start_time = time.time()
+    
+    iterator = data.train_data.iterrows()
+    window = np.zeros((0,29))
+    
+    progress = IntProgress(
+        min=0,
+        max=data.ntrain,
+        step=1,
+        description='Training:',
+        bar_style='danger'
+    )
+    display(progress)
+    
+    for idx, row in enumerate(iterator):
+        window = np.append(window, [row[1]], axis=0)
+        if window.shape[0] == window_size:
+            train_on_window(preprocessors, apply_pca, classifier, window)
+            window = np.zeros((0,29))
+            progress.value = idx
+    if len(window) > 0:
+        train_on_window(preprocessors, apply_pca, classifier, window)
+        progress.value = data.ntrain
+
+    print('Time taken: {}'.format(time.time() - start_time))
+
+def training_ui(data, sample_data, preprocessors, classifiers):
+    use_sample_data = Checkbox(
+        value=False,
+        description='Use fast sample data'
+    )
+    display(use_sample_data)
+
+    pca_checkbox = Checkbox(
+        value=False,
+        description='Apply PCA'
+    )
+    display(pca_checkbox)
+
+    window_size_slider = IntSlider(
+        value=500,
+        min=100,
+        max=1000,
+        step=100,
+        description='window size:',
+        continuous_update=False,
+        layout=Layout(width='80%')
+    )
+    display(window_size_slider)
+
+    classifier_rb = RadioButtons(
+        options=['Multilayer Perceptron', 'Naive Bayes'],
+        description='Classifier:'
+    )
+    display(classifier_rb)
+
+    start_training = Button(
+        description='Start training',
+        button_style='danger'
+    )
+    display(start_training)
+
+    def training_func(*args):
+        if use_sample_data.value:
+            d = sample_data
+        else:
+            d = data
+
+        perform_training(
+            data=d, 
+            preprocessors=preprocessors, 
+            apply_pca=pca_checkbox.value, 
+            window_size=window_size_slider.value,
+            classifiers=classifiers,
+            classifier_name=classifier_rb.value)
+
+    start_training.on_click(training_func)
+
+
+#### Prediction ####
+
+def predict_on_window(preprocessors, apply_pca, classifier, window):
+    features = window[:, 1:29]
+    
+    # Optionally apply PCA
+    if apply_pca:
+        features = preprocessors['pca'].transform(features)
+    
+    # Predict
+    prediction = classifier.predict(features)
+    return prediction
+
+def perform_prediction(
+    data, preprocessors, apply_pca, window_size, classifiers, 
+    classifier_name):
+    
+    # Load classifier
+    if classifier_name == 'Multilayer Perceptron':
+        classifier = classifiers['mlp']['trained']
+    else:
+        classifier = classifiers['nb']['trained']
+
+    print('Predict with {}'.format(classifier))
+    
+    iterator = data.test_data.iterrows()
+    window = np.zeros((0,29))
+    conf_mat = np.zeros((2, 2))
+    
+    progress = IntProgress(
+        min=0,
+        max=data.ntest,
+        step=1,
+        description='Predicting:',
+        bar_style='info'
+    )
+    display(progress)
+    
+    for idx, row in enumerate(iterator):
+        window = np.append(window, [row[1]], axis=0)
+        if window.shape[0] == window_size:
+            prediction = predict_on_window(
+                preprocessors, apply_pca, classifier, window)
+            conf_mat += confusion_matrix(window[:,0], prediction)
+            window = np.zeros((0,29))
+            progress.value = idx
+    if len(window) > 0:
+        prediction = predict_on_window(
+            preprocessors, apply_pca, classifier, window)
+        conf_mat += confusion_matrix(window[:,0], prediction)
+        progress.value = data.ntest
+
+    print(conf_mat)
+
+def prediction_ui(data, sample_data, preprocessors, classifiers):
+    use_sample_data = Checkbox(
+        value=False,
+        description='Use fast sample data'
+    )
+    display(use_sample_data)
+
+    pca_checkbox = Checkbox(
+        value=False,
+        description='Apply PCA'
+    )
+    display(pca_checkbox)
+
+    window_size_slider = IntSlider(
+        value=500,
+        min=100,
+        max=1000,
+        step=100,
+        description='window size:',
+        continuous_update=False,
+        layout=Layout(width='80%')
+    )
+    display(window_size_slider)
+
+    classifier_rb = RadioButtons(
+        options=['Multilayer Perceptron', 'Naive Bayes'],
+        description='Classifier:'
+    )
+    display(classifier_rb)
+
+    start_prediction = Button(
+        description='Start prediction',
+        button_style='info'
+    )
+    display(start_prediction)
+
+    def prediction_func(*args):
+        if use_sample_data.value:
+            d = sample_data
+        else:
+            d = data
+
+        perform_prediction(
+            data=d, 
+            preprocessors=preprocessors, 
+            apply_pca=pca_checkbox.value, 
+            window_size=window_size_slider.value,
+            classifiers=classifiers,
+            classifier_name=classifier_rb.value)
+
+    start_prediction.on_click(prediction_func)
